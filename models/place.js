3e4713d83531301daa9bcc3d7178858bf855f857
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-06-14 10:31:17 +0530

    Initial commit

---
+++ models/place.js
@@ -0,0 +1,11 @@
+class Place {
+  constructor(id, title, imageUrl, lat, lng) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
+
+export default Place;
