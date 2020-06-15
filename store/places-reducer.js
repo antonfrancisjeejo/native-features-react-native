f5de9dfc1d296b7de327ad14bd450b3198d58ba2
Anton Francis Jeejo <anton13jeejo@gmail.com>
2020-06-15 19:37:17 +0530

    Initial commit

---
+++ store/places-reducer.js
@@ -0,0 +1,31 @@
+import { ADD_PLACE, SET_PLACES } from "./places-actions";
+import Place from "../models/place";
+
+const initialState = {
+  places: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_PLACE:
+      const newPlace = new Place(
+        action.placeData.id.toString(),
+        action.placeData.title,
+        action.placeData.image,
+        action.placeData.coords.lat,
+        action.placeData.coords.lng
+      );
+      return {
+        places: state.places.concat(newPlace),
+      };
+    case SET_PLACES:
+      return {
+        places: action.places.map(
+          (pl) =>
+            new Place(pl.id.toString(), pl.title, pl.imageUrl, pl.lat, pl.lng)
+        ),
+      };
+    default:
+      return state;
+  }
+};
